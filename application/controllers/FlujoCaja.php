1b90e6fa6461bc2d429497fdcb37935a040e4e13
Alejandro Rothar <arothar@gmail.com>
2015-01-08 17:36:17 -0300

    Anteriormente no habia subido los archivos nuevos. Ahora los estoy subiendo.

---
+++ application/controllers/FlujoCaja.php
@@ -0,0 +1,55 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class FlujoCaja extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->helper('url');
+		$this->load->helper('form');
+		$this->load->model('TipoMovimiento','',TRUE);
+		$this->load->model('Evento','',TRUE);
+		$this->load->model('Movimiento','',TRUE);
+
+	}
+
+	public function index()
+	{
+		$out = $this->load->view('view_calendarCaja.php', NULL, TRUE);
+		$data['cuerpo'] = $out;
+		$this->load->view('view_template.php', $data);
+	}
+
+	public function getMovimientosDelMes(){
+		$movimientosDelMes = $this->Movimiento->get_saldoCalendario(1,2015);
+
+		foreach ($movimientosDelMes as $key => $value) {
+			$objeto = new Evento();
+			$objeto->title= $value->acumulado;
+			$objeto->start = $value->fechaPago;
+			
+			
+
+			if (sizeof($movimientosDelMes) > $key +1 ){
+				$proximo = $movimientosDelMes[$key+1];
+				$objeto->end= strtotime ( '-0 day' , strtotime ( $proximo->fechaPago) ) ;	
+			}else{
+				$objeto->end=$value->fechaPago;
+			}
+			if ($value->acumulado > 0)
+				$objeto->backgroundColor= "#33C276";
+			else 
+				$objeto->backgroundColor= "#D23C25";
+
+			$objeto->url = base_url() . 'index.php/IngresoMovimiento/traerMovimientos/' . date("Y-m-d",strtotime($objeto->start));
+
+			$dato[$key] = $objeto;
+		}
+
+		echo json_encode($dato);
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
